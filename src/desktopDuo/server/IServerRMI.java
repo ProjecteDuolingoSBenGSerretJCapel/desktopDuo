bb3fc9e715330179145db77155ea4f4933b4d656
JCapelDev <win200003@gmail.com>
2020-12-04 17:12:43 +0100

    Server del desktop

---
+++ src/desktopDuo/server/IServerRMI.java
@@ -0,0 +1,5 @@
+package desktopDuo.server;
+
+public interface IServerRMI {
+
+}
---
+++ src/desktopDuo/server/ServidorDesktopDuo.java
@@ -0,0 +1,20 @@
+package desktopDuo.server;
+
+
+
+public class ServidorDesktopDuo implements IServerRMI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public ServidorDesktopDuo() {
+		try {
+			CallHandler callHandler = new CallHandler();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+}
